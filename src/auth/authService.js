e57f1656ffc0bceed3e8362c39e4bdf9315c2eae
Jorge Castellon <jorge@thoriumdesign.com>
2020-02-11 19:07:39 -0800

    moved auth_config to src folder

--- src/auth/authService.js
+++ src/auth/authService.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import auth0 from "auth0-js";
 import { EventEmitter } from "events";
-import authConfig from "../../auth_config.json";
+import authConfig from "../auth_config.json";
 
 const webAuth = new auth0.WebAuth({
   domain: authConfig.domain,
