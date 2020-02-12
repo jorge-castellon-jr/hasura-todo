a87c7714b79978430d5c80a7dc19350aff866a39
Jorge Castellon <jorge@thoriumdesign.com>
2020-02-11 18:05:40 -0800

    first commit added auth0 and heroku

---
+++ src/main.js
@@ -0,0 +1,54 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import AuthPlugin from "./plugins/auth";
+
+import VueApollo from "vue-apollo";
+
+import ApolloClient from "apollo-client";
+import { WebSocketLink } from "apollo-link-ws";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+Vue.use(AuthPlugin);
+Vue.use(VueApollo);
+
+Vue.config.productionTip = false;
+
+const getHeaders = () => {
+  const headers = {};
+  const token = window.localStorage.getItem('apollo-token');
+  if (token) {
+    headers.authorization = `Bearer ${token}`;
+  }
+  return headers;
+};
+
+const link = new WebSocketLink({
+  uri: "wss://tutorial-hasura-backend.herokuapp.com/v1/graphql",
+  options: {
+    reconnect: true,
+    timeout: 30000,
+    connectionParams: () => {
+      return {
+        headers: getHeaders()
+      }
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache({
+    addTypename: true
+  })
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: client,
+})
+
+new Vue({
+  router,
+  apolloProvider,
+  render: h => h(App)
+}).$mount("#app");
